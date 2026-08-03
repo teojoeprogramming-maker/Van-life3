62a7702a6332d1a06a60fbdaadeeacd92adce56a
Super Teo <superteo@Supers-MacBook-Air.local>
2026-08-03 16:36:06 -0500

    h1 fix

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,7 +6,7 @@ function Home(){
         <>
           <Navbar></Navbar>
           <div id='imageDiv'>
-            <h1 id='you'> got the travel plans, we got the travel vans</h1>
+            <h1 id='you'>You got the travel plans, we got the travel vans</h1>
             <h3 id='add'>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</h3>
             <button id='findButton'>Find your van</button>
             </div>
